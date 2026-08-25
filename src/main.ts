59c660c5cdbeecd422fd68dd1e243909ef7852c4
Faiz2501 <faizfiroz2501@gmail.com>
2026-08-25 14:49:26 +0530

    import dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import helmet from 'helmet';
-
+import 'dotenv/config' 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
